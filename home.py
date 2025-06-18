47d527821a632c0312d977401d21aa7759d5128b
Haris Ali <malikharis2003@gmail.com>
2025-06-18 12:45:33 +0500

    Make a simple homepage

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center;'>Welcome to the Civics Quiz!</h1>", unsafe_allow_html=True)
+st.text('\n\n')
+col1, col2, col3 = st.columns([1, 1, 1])
+with col2:
+    start_btn = st.button('Start Quiz!')
+
+if start_btn:
+    st.switch_page('questions.py')
